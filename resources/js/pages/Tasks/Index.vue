fda1e8dada5eda6008f06de92464cc9d952baf89
Modestas Vaitkevičius <info@modestasv.lt>
2025-03-14 10:51:54 +0200

    Task CRUD Toast notification (lesson 5)

--- resources/js/pages/Tasks/Index.vue
+++ resources/js/pages/Tasks/Index.vue
@@ -2,13 +2,22 @@
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { Task } from '@/types';
-import { Head } from '@inertiajs/vue3';
+import { Head, router } from '@inertiajs/vue3';
+import { Button } from '@/components/ui/button';
+import { toast } from 'vue-sonner';
 
 interface Props {
     tasks: Task[];
 }
 
 defineProps<Props>();
+
+const deleteTask = (id: number) => {
+    if (confirm('Are you sure you want to delete this task?')) {
+        router.delete(route('tasks.destroy', { id }));
+        toast.success('Task deleted successfully');
+    }
+};
 </script>
 
 <template>
@@ -28,7 +37,12 @@ defineProps<Props>();
                     <TableCell class="{{ task.is_completed ? 'text-green-600' : 'text-red-700'}}">
                         {{ task.is_completed ? 'Completed' : 'In Progress' }}
                     </TableCell>
-                    <TableCell class="text-right">Button to edit/delete</TableCell>
+                    <TableCell class="text-right">
+                        <Button
+                            variant="destructive"
+                            @click="deleteTask(task.id)"
+                            class="mr-2">Delete</Button>
+                    </TableCell>
                 </TableRow>
             </TableBody>
         </Table>
