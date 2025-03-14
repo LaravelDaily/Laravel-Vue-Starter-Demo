681182225c5a9dac33c3581a90390dd3663fd52e
Modestas Vaitkevičius <info@modestasv.lt>
2025-03-14 13:41:55 +0200

    Added task edit page (lesson 7)

--- resources/js/pages/Tasks/Index.vue
+++ resources/js/pages/Tasks/Index.vue
@@ -3,7 +3,7 @@ import { Button, buttonVariants } from '@/components/ui/button';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { Task } from '@/types';
-import { Head, router, Link } from '@inertiajs/vue3';
+import { Head, Link, router } from '@inertiajs/vue3';
 import { toast } from 'vue-sonner';
 
 interface Props {
@@ -25,7 +25,7 @@ const deleteTask = (id: number) => {
         <Head title="Tasks List" />
 
         <div class="mt-4">
-            <Link :class="buttonVariants({variant: 'outline'})" href="/tasks/create"> Create Task </Link>
+            <Link :class="buttonVariants({ variant: 'outline' })" href="/tasks/create"> Create Task</Link>
         </div>
 
         <Table class="mt-4">
@@ -33,7 +33,7 @@ const deleteTask = (id: number) => {
                 <TableRow>
                     <TableHead>Task</TableHead>
                     <TableHead class="w-[100px]">Status</TableHead>
-                    <TableHead class="w-[100px] text-right">Actions</TableHead>
+                    <TableHead class="w-[200px] text-right">Actions</TableHead>
                 </TableRow>
             </TableHeader>
             <TableBody>
@@ -42,7 +42,8 @@ const deleteTask = (id: number) => {
                     <TableCell :class="{ 'text-green-600': task.is_completed, 'text-red-700': !task.is_completed }">
                         {{ task.is_completed ? 'Completed' : 'In Progress' }}
                     </TableCell>
-                    <TableCell class="text-right">
+                    <TableCell class="flex gap-x-2 text-right">
+                        <Link :class="buttonVariants({ variant: 'default' })" :href="`/tasks/${task.id}/edit`">Edit </Link>
                         <Button variant="destructive" @click="deleteTask(task.id)" class="mr-2">Delete </Button>
                     </TableCell>
                 </TableRow>
