cfc57d00269f270bd7a8cae41d666183b4f117d3
Modestas Vaitkevičius <info@modestasv.lt>
2025-03-14 09:53:43 +0200

    Task CRUD backend (lesson 3)

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Task>
+ */
+class TaskFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'is_completed' => fake()->boolean(),
+        ];
+    }
+}
